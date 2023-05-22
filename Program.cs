3778406b9efb77427f6071ff99c548edf7a33542
kaidathulipriyanka <kaidhathulipriyanka@gmail.com>
2023-05-22 12:26:27 +0530

    addition code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace practice1
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("addition");
+            int a = 10, b = 20;
+            Console.WriteLine(a+b);
+        }
+    }
+}
